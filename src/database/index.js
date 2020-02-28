8f417e13ce42644a93f9765e8c6d83fc1378774e
Cassio C <LccassioL@gmail.com>
2020-02-28 17:25:57 -0300

    Create delivery problems migration

--- src/database/index.js
+++ src/database/index.js
@@ -4,10 +4,11 @@ import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
 import File from '../app/models/File';
 import Delivery from '../app/models/Delivery';
+import DeliveryProblems from '../app/models/DeliveryProblems';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient, File, Delivery];
+const models = [User, Recipient, File, Delivery, DeliveryProblems];
 
 class Database {
   constructor() {
